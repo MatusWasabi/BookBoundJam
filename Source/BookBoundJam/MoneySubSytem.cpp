f76f800eab314cd6a5a1cf55eb2d21bdf82e2d04
Matus Yaowvasrisuwan <matus.y@kkumail.com>
2023-12-14 14:47:07 +0700

    Buying Seed System

---
+++ Source/BookBoundJam/MoneySubSytem.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MoneySubSytem.h"
+
+void UMoneySubSytem::Initialize(FSubsystemCollectionBase& Collection)
+{
+	Super::Initialize(Collection);
+}
+
+void UMoneySubSytem::Deinitialize()
+{
+	Super::Deinitialize();
+}
+
+void UMoneySubSytem::Increase(int Amount)
+{
+	Money += Amount;
+}
+
+void UMoneySubSytem::Decrease(int Amount)
+{
+	Money -= Amount;
+}
+
+int UMoneySubSytem::GetPlayerMoney()
+{
+	return Money;
+}
+
---
+++ Source/BookBoundJam/MoneySubSytem.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "MoneySubSytem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOOKBOUNDJAM_API UMoneySubSytem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+
+
+public:
+	// Begin USubsystem
+	virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+	virtual void Deinitialize() override;
+	// End USubsystem
+
+	UFUNCTION(BlueprintCallable, Category = "Money")
+	void Increase(int Amount);
+	UFUNCTION(BlueprintCallable, Category = "Money")
+	void Decrease(int Amount);
+
+	UFUNCTION(BlueprintPure)
+	int GetPlayerMoney();
+	
+private:
+	// All my variables
+	int Money = 0;
+};
